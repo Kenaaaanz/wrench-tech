40c619e6c94628149ac0ccb18e5c60d38c177e5d
Kenaaaanz <gichabakenani@gmail.com>
2025-06-24 20:25:50 +0300

    Update settings.py

--- wrenchshop/settings.py
+++ wrenchshop/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'wrenchshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-  DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
     )
